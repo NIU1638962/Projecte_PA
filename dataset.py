05dd8d6c93e6d958836be01148512071d2ece021
akselserret <104071433+akselserret@users.noreply.github.com>
2022-04-30 13:19:40 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 20 13:00:34 2022
-
-@author: Joel Tapia Salvador
-"""
-# -*- coding: utf-8 -*-
-"""
 Created on Fri Apr 29 10:39:22 2022
 
 @author: marcs
@@ -14,13 +8,13 @@ from dataclasses import dataclass, field
 from typing import List, Dict
 from datetime import date, datetime, timedelta
 from abc import ABCMeta, abstractmethod
-
+import numpy as np
 @dataclass
 class Dataset:
     def __init__(self):
         self._usuarios=0
         self._valoraciones=[]
-        self._elementos=[]
+        self._elementos={}
     @property
     def usuarios(self):
         return self._usuarios
@@ -38,17 +32,18 @@ class Dataset:
                 if self._valoraciones[columna, i]!=0:
                     recuento+=1
                     suma+=self._valoraciones[columna, i]
+        media_global=suma/size[1]
         media=suma/recuento
-        score=(recuento/(recuento+min_votos))
+        score=(recuento/(recuento+min_votos)*media)+(min_votos/(recuento+min_votos)*media_global)
         return score
     
-    def menu(self):
-        print("Elija una opción")
-        print("Opción 1: listado de valoraciones.")
-        print("Opción 2: añadir valoración.")
     
     def top_popular_items (self, min_votos):
         score_dict={}
+        repeticion="s"
+        print("Elija una opción")
+        print("Opción 1: listado de recomendaciones.")
+        print("Opción 2: añadir valoraciones.")        
         opcion=input()
         if opcion==1:
             for elemento in self._elementos:
@@ -56,5 +51,25 @@ class Dataset:
                 for columna in range(size[0]):
                     score=self.score_top_popular_items(min_votos, columna)                
                     score_dict[elemento]=score
+            score_dict = sorted(score_dict.items(), key=lambda x: x[1])
+            return score_dict
         elif opcion==2:
-            pelicula=input()
+            dict_valoracion={}
+            repeticion="s"
+            while repeticion=="s":
+                pelicula=input("Introduzca el titulo de la película: ")
+                if pelicula not in self._elementos.keys():
+                    raise AssertionError("Película no disponible")
+                else:
+                    valoracion_usuario=float(input("Introduzca su valoración: "))
+                    columna=self._elementos.keys().index(pelicula)
+                    dict_valoracion[pelicula]=valoracion_usuario
+                    repeticion=input("¿Quiere valorar otra película?(s/n):")
+            nuevo_usuario=np.zeros(size[1])  
+            for columna, valoracion in dict_valoracion.values():
+                nuevo_usuario[1,columna]=valoracion
+                score=self.score_top_popular_items(min_votos, columna)
+                score_dict[pelicula]=score
+            score_dict = sorted(score_dict.items(), key=lambda x: x[1])
+            return score_dict
+        
