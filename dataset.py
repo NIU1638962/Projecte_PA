438b79e1422d8dc73296cd0ef413f335a60c3a8c
akselserret <104071433+akselserret@users.noreply.github.com>
2022-05-05 20:25:54 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -41,29 +41,33 @@ class Dataset(metaclass=ABCMeta):
     @property
     def elementos(self):
         return self._elementos
-        def score_others_users_also(self, k, usuario):
+    def score_other_users_also(self, k, usuario):
         valoraciones_usuario=self._valoraciones[usuario,:]
         dict_similitudes={}
-        for fila in range(self._filas-1):
+        for fila in range(self._filas):
             if fila!=usuario:
-                valoraciones_else=self._valoraciones[fila,:]
+                valoraciones_else=(self._valoraciones[fila,:])
                 suma=0
                 den1=[]
                 den2=[]
-                for usu1, usu2 in (valoraciones_usuario, valoraciones_else):
-                    if usu1!=0 and usu2!=0:
-                        suma+=(usu1*usu2)
-                        den1.append(usu1)
-                        den2.append(usu2)
-                den1=[n**2 for n in den1]
+                for i in range(self._columnas):
+                    usu1=valoraciones_usuario[0, i]
+                    usu2=valoraciones_else[0,i]
+                    if not np.array_equal(usu1,0) and  not np.array_equal(usu2,0) :
+                        suma+=(valoraciones_usuario[0, i])*valoraciones_else[0,i]
+                        den1.append(valoraciones_usuario[0, i])
+                        den2.append(valoraciones_else[0,i])
+                den1=[n**2 for n in den1]                
                 den2=[n**2 for n in den2]
-                similitud=suma/(((sum(den1))**(1/2))*(sum(den2))**(1/2))
-                dict_similitudes[fila]=similitud
+                den1=(sum(den1))
+                den2=sum(den2)
+                if den1!=0 or den2!=0:                    
+                    similitud=suma/((((den1))**(1/2))*((den2))**(1/2))
+                    dict_similitudes[fila]=similitud
         dict_similitudes = sorted(dict_similitudes.items(), key=lambda x: x[1], reverse=True)
-        lista_similitudes=dict_similitudes.items()
+        lista_similitudes=dict_similitudes
         usuarios_mas_parecidos=lista_similitudes[:k]
         return usuarios_mas_parecidos
-    
     def other_users_also(self, k, usuario):
        score_dict={}
        usuarios_parecidos=self.score_other_users_also(k, usuario)
@@ -72,35 +76,44 @@ class Dataset(metaclass=ABCMeta):
            recuento=0
            suma=0
            for fila in usuarios_parecidos:
-               if self._valoraciones[usuario[0],columna]!=0:
+               if self._valoraciones[fila[0], columna]!=0:
                    recuento+=1
-                   suma+=self._valoraciones[usuario[0], columna]
+                   suma+=self._valoraciones[fila[0], columna]
            if recuento==0:
                media=0
            else:               
                media=suma/recuento
                medias.append(medias)
-       for media, index in zip(medias, range(self._columnas[1]-1)):       
+       for media, index in zip(medias, range(self._columnas)):       
           titulo=(self._elementos[0][index]._titol)
           score_dict[titulo]=media
-       indice=[]
        indices=[]
-       for valoraciones in (self._valoraciones[usuario]):
-          if valoraciones==0:
-              indice.append(indice)
+       for i in range(self._columnas):
+           print(self._valoraciones[usuario, i])
+           if self._valoraciones[usuario, i]==0:
+              
+              indices.append(i)
+       print(indices)
        recomendadas=list(score_dict.items())
        peliculas_recomendadas=[]
        for indice in indices:
            peliculas_recomendadas.append(recomendadas[indice])
        peliculas_recomendadas.sort(key = lambda x: x[1], reverse=True)
        return peliculas_recomendadas
-
-            
+              
+              
+         
+    
+           
+        
+    
+    
     def top_popular_items (self, min_votos, usuario):
         score_dict={}
         medias=[]
         votos=[]
         valoraciones=[]
+        print(self._columnas)
         size=self._valoraciones.shape
         for columna in range(size[1]):
             recuento=0
@@ -127,43 +140,25 @@ class Dataset(metaclass=ABCMeta):
                 score=(recuento/(recuento+min_votos)*media)+(min_votos/(recuento+min_votos)*media_global)
             valoraciones.append(score)
 
-        for score, index in zip(valoraciones, range(size[1]-1)):       
+        for score, index in zip(valoraciones, range(self._columnas)):       
             titulo=(self._elementos[0][index]._titol)
             score_dict[titulo]=score
+            print(index)
         # score_dict = sorted(score_dict.items(), key=lambda x: x[1])
+        print(len(score_dict))
         valoraciones_usuario=self._valoraciones.getrow(usuario)
         indices=[]
         rec_usuario={}
-        indices=valoraciones_usuario.nonzero()
-        print(len(indices[1]))
-        for index in indices[1]:
+        indices=[i for i in range(self._columnas + 1) if i not in list(valoraciones_usuario.nonzero()[1])]
+        # indices=np.nonzero(valoraciones_usuario == 0) 
+        print(len(indices))
+        for index in indices:
             titol=list(score_dict.keys())
             titol=titol[index]
             rec_usuario[titol]=score_dict[titol]
+
         rec_usuario = sorted(rec_usuario.items(), key=lambda x: x[1], reverse=True)
-        print(len(rec_usuario))
         return rec_usuario
-                
-            
-        
-        # elif opcion==2:
-        #     dict_valoracion={}
-        #     repeticion="s"
-        #     while repeticion=="s":
-        #         pelicula=input("Introduzca el titulo de la película: ")
-        #         if pelicula not in self._elementos.keys():
-        #             raise AssertionError("Película no disponible")
-        #         else:
-        #             valoracion_usuario=float(input("Introduzca su valoración: "))
-        #             columna=self._elementos.keys().index(pelicula)
-        #             dict_valoracion[pelicula]=valoracion_usuario
-        #             repeticion=input("¿Quiere valorar otra película?(s/n):")
-        #     nuevo_usuario=np.zeros(size[1])  
-        #     for columna, valoracion in dict_valoracion.values():
-        #         nuevo_usuario[1,columna]=valoracion
-        #         score=self.score_top_popular_items(min_votos, columna)
-        #         score_dict[pelicula]=score
-        #     score_dict = sorted(score_dict.items(), key=lambda x: x[1])
     @abstractmethod
     def read_data(self, directory: str, names_files: List[str]):
         raise NotImplementedError()
