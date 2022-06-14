787a9d361b69ba1d25b3716ff4fdd65bf55bbddb
Joel Tapia Salvador <91463596+NIU1638962@users.noreply.github.com>
2022-06-14 13:23:25 +0200

    Update 14062022a.

--- recomanador.py
+++ recomanador.py
@@ -12,6 +12,12 @@ from películes import Pelicules
 from board_games import BoardGames
 from data import Data
 import console_messages as co
+from recomanacions import (
+    Recomanacions,
+    Recom_top_popular,
+    Recom_other_users,
+    Recom_you_liked,
+)
 
 
 logging.basicConfig(
@@ -24,147 +30,139 @@ logging.basicConfig(
 N_RECOMANACIONS = 5
 MIN_VOTS = 10
 K_USUARIS = 10
-DIRECTORY_PEL = "ml-latest-small"
-NAMES_FILES_PEL = ("movies.csv", "ratings.csv")
-DIRECTORY_GAM = "archive_reduced"
-NAMES_FILES_GAM = (
-    "games.csv",
-    "mechanics.csv",
-    "subcategories.csv",
-    "user_ratings.csv",
-)
 
 
 def recomanador():
+    pickle = True
+    opcio_dataset = None
+    opcio_recomanacio = None
+    recomanacions = None
+    usuari = None
+    recomanacio = None
     try:
         logging.info("S'ha iniciat el programa recomanació.")
-        opcio_dataset = None
-        dataset = None
-        while opcio_dataset != 3:
+        while True:
             menu_select_dataset()
             try:
-                opcio_dataset = int(input(co.cgray("Selecciona opció: ")))
+                opcio_dataset = input(co.cgray("Selecciona opció: "))
+                if opcio_dataset in "Ss":
+                    print(co.clblue("Sortint del programa..."))
+                    recomanacions = None
+                    logging.debug(
+                        "Opcio cleared i sortint del programa.\n\t%s", opcio_dataset()
+                    )
+                    break
+                opcio_dataset = int(opcio_dataset)
+                logging.debug("Opció dataset escollida: %s", opcio_dataset)
             except ValueError as message:
                 print(co.cdred("Opció ha de ser un enter postiu."))
                 logging.warning(str(message))
-            else:
-                logging.debug("Opció dataset escollida: %s", opcio_dataset)
-                if opcio_dataset == 1:
-                    dataset = Pelicules(DIRECTORY_PEL, NAMES_FILES_PEL)
-                    logging.debug(
-                        "Incialitzat 'dataset' a  objecte 'Películes'.\n\t%s", dataset
-                    )
-                elif opcio_dataset == 2:
-                    dataset = BoardGames(DIRECTORY_GAM, NAMES_FILES_GAM)
-                    logging.debug(
-                        "Incialitzat 'dataset' a  objecte 'Board_Games'.\n\t%s", dataset
-                    )
-                elif opcio_dataset == 3:
-                    print(co.clblue("Sortint del programa..."))
-                    dataset = None
-                    logging.debug(
-                        "Dataset cleared i sortint del programa.\n\t%s", dataset
-                    )
+            opcio_recomanacio = None
+            usuari = None
+            while opcio_recomanacio != 4:
+                menu_select_recomanacio()
+                try:
+                    opcio_recomanacio = int(input(co.cgray("Selecciona una opció: ")))
+                except ValueError as message:
+                    print(co.cdred("Opció ha de ser un enter postiu."))
+                    logging.warning(str(message))
                 else:
-                    print(co.cdred("Error: Seleccioni opció vàlida."))
-                    opcio_dataset = None
-                    dataset = None
-                    logging.warning(
-                        "Dataset i opció dataset cleared, opció no correcta.\n\t%s\n\t%s",
-                        opcio_dataset,
-                        dataset,
-                    )
-                if dataset is not None:
-                    print(co.clblue("Carregant base de dades..."))
-                    dataset.read_data()
-                    print(co.clblue("Base de dades carregada."))
-                    opcio_recomanacio = None
-                    usuari = None
-                    while opcio_recomanacio != 4:
-                        menu_select_recomanacio()
-                        try:
-                            opcio_recomanacio = int(
-                                input(co.cgray("Selecciona una opció: "))
+                    logging.debug("Opció recomanació escollida: %s", opcio_recomanacio)
+                    try:
+                        if opcio_recomanacio == 1:
+                            print(co.clblue("Carregant recomanacions..."))
+                            recomanacions = Recom_top_popular(
+                                opcio_dataset,
+                                N_RECOMANACIONS,
+                                MIN_VOTS,
+                                K_USUARIS,
+                                pickle,
                             )
-                        except ValueError as message:
-                            print(co.cdred("Opció ha de ser un enter postiu."))
-                            logging.warning(str(message))
+                            print(co.clblue("Recomanacions carregades."))
+                        elif opcio_recomanacio == 2:
+                            print(co.clblue("Carregant recomanacions..."))
+                            recomanacions = Recom_other_users(
+                                opcio_dataset,
+                                N_RECOMANACIONS,
+                                MIN_VOTS,
+                                K_USUARIS,
+                                pickle,
+                            )
+                            print(co.clblue("Recomanacions carregades."))
+                        elif opcio_recomanacio == 3:
+                            print(co.clblue("Carregant recomanacions..."))
+                            recomanacions = Recom_you_liked(
+                                opcio_dataset,
+                                N_RECOMANACIONS,
+                                MIN_VOTS,
+                                K_USUARIS,
+                                pickle,
+                            )
+                            print(co.clblue("Recomanacions carregades."))
+                        elif opcio_recomanacio == 4:
+                            print(co.clblue("Sortint del menú..."))
+                            recomanacions = None
+                            opcio_recomanacio = None
+                            logging.info(
+                                "Opció recomanació i recomanacio cleared, opció no correcta.\n\t%s\n\t%s",
+                                opcio_recomanacio,
+                                recomanacions,
+                            )
                         else:
-                            logging.debug(
-                                "Opció recomanació escollida: %s", opcio_recomanacio
+                            print(co.cdred("Error: Seleccioni opció vàlida."))
+                            opcio_recomanacio = None
+                            recomanacions = None
+                            logging.warning(
+                                "Opció recomanació i recomanacio cleared, opció no correcta.\n\t%s\n\t%s",
+                                opcio_recomanacio,
+                                recomanacions,
                             )
-                            if opcio_recomanacio in (1, 2, 3):
-                                usuari = input(
-                                    co.cgray(
-                                        "Selecciona un usuari entre 0-"
-                                        + str(dataset.filas - 1)
-                                        + " : "
-                                    )
+                    except AssertionError as message:
+                        print(co.cdred(str(message)))
+                        logging.warning(str(message))
+                    except NotImplementedError as message:
+                        print(co.cdred(str(message)))
+                        logging.warning(str(message))
+                    else:
+                        while usuari != "":
+                            usuari = input(
+                                co.cgray(
+                                    "Selecciona un usuari entre 0-"
+                                    + str(recomanacions.dataset.filas - 1)
+                                    + " : "
                                 )
-                                logging.debug("Usuari escollid: %s", usuari)
-                                while usuari != "":
-                                    recomanacio = None
-                                    try:
-                                        usuari = int(usuari)
-                                    except ValueError as message:
-                                        print(
-                                            co.cdred(
-                                                "Usuari ha de ser un enter postiu."
-                                            )
-                                        )
-                                        logging.warning(str(message))
-                                    else:
-                                        try:
-                                            if opcio_recomanacio == 1:
-                                                recomanacio = dataset.top_popular_items(
-                                                    MIN_VOTS, int(usuari)
-                                                )
-                                            elif opcio_recomanacio == 2:
-                                                recomanacio = dataset.other_users_also(
-                                                    K_USUARIS, int(usuari)
-                                                )
-                                            elif opcio_recomanacio == 3:
-                                                raise NotImplementedError(
-                                                    "Opció no implementada."
-                                                )
-                                        except AssertionError as message:
-                                            print(co.cdred(str(message)))
-                                            logging.warning(str(message))
-                                        except NotImplementedError as message:
-                                            print(co.cdred(str(message)))
-                                            logging.warning(str(message))
-                                        else:
-                                            logging.debug(
-                                                "Recomanacions: %s", recomanacio
-                                            )
-                                            visualitza_rec(
-                                                recomanacio[:N_RECOMANACIONS]
-                                            )
-                                            recomanacio = None
-                                    usuari = input(
-                                        co.cgray(
-                                            "Selecciona un usuari entre 0-"
-                                            + str(dataset.filas - 1)
-                                            + " : "
-                                        )
-                                    )
-                                    logging.debug("Usuari escollid: %s", usuari)
-                            elif opcio_recomanacio == 4:
-                                print(co.clblue("Sortint del menú..."))
-                                dataset = None
-                                logging.info(
-                                    "Dataset cleared i sortint de la selecció de recomanació.\n\t%s",
-                                    dataset,
+                            )
+                            logging.debug("Usuari escollid: %s", usuari)
+                            if usuari == "":
+                                print(co.clblue("Sortint de selecció d'usuari..."))
+                                recomanacio = None
+                                usuari = None
+                                logging.debug(
+                                    "Usuari i  recomanacio cleared, sortint del programa.\n\t%s\n\t%s",
+                                    usuari,
+                                    recomanacio,
                                 )
+                                break
+                            try:
+                                usuari = int(usuari)
+                            except ValueError as message:
+                                print(co.cdred("Usuari ha de ser un enter postiu."))
+                                logging.warning(str(message))
                             else:
-                                print(co.cdred("Error: Seleccioni opció vàlida."))
-                                opcio_recomanacio = None
-                                logging.warning(
-                                    "Opció recomanació cleared, opció no correcta.\n\t%s",
-                                    opcio_dataset,
-                                )
+                                recomanacio = recomanacions.recomana(usuari)
+                                logging.debug("Recomanacions: %s", recomanacio)
+                                visualitza_rec(recomanacio)
+                                recomanacio = None
     except:
-        logging.error("S'ha terminat el programa recomanació. \n\t%s", dataset)
+        logging.error(
+            "S'ha terminat el programa recomanació. \n\t%s\n\t%s\n\t%s\n\t%s\n\t%s\n\t%s",
+            pickle,
+            opcio_dataset,
+            opcio_recomanacio,
+            recomanacions,
+            usuari,
+            recomanacio,
+        )
         exc_tuple = sys.exc_info()
         type_error = str(exc_tuple[0]).split()[1][:-1]
         message = str(exc_tuple[1])
